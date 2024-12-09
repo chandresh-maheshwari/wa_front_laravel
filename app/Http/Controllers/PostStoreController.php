315a6428b4e48a196a23e4b74918806f51d7c807
chandresh <chandresh.maheshwari@gmail.com>
2024-12-09 10:56:36 +0530

    s

--- app/Http/Controllers/PostStoreController.php
+++ app/Http/Controllers/PostStoreController.php
@@ -119,8 +119,10 @@ class PostStoreController extends Controller
                 $normalizedLabel = str_replace(' ', '_', $field['label']);
                 if ($field['type'] === 'file' && $request->hasFile($normalizedLabel)) {
                     $file = $request->file($normalizedLabel);
-                    $base64Image = base64_encode(file_get_contents($file->getRealPath()));
-                    $data[$field['label']] = $base64Image;
+                    $originalName = $file->getClientOriginalName();
+                    $uploadFolder = 'uploads/dynamic_post_store';
+                    $file->move(public_path($uploadFolder), $originalName);
+                    $data[$field['label']] = URL::to($uploadFolder . '/' . $originalName);
                 }
             }
 
