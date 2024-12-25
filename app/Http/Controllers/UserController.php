73659ec8ef0a4706acbbf6574125b3adef1ae916
chandresh <chandresh.maheshwari@gmail.com>
2024-12-25 11:45:31 +0530

    issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\AccountInformationModel;
-use App\Models\AddressAccountInformationModel;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+
 use Illuminate\Support\Facades\Session;
-use App\Models\ProductModel;
+
 
 class UserController extends Controller
 {
@@ -17,10 +15,10 @@ class UserController extends Controller
         $this->middleware('user');
     }
 
-    public function index()    {       
-        AccountInformationModel::destroy('user');
-        return view('homepage');
-    }    
+    // public function index()    {       
+    //     AccountInformationModel::destroy('user');
+    //     return view('homepage');
+    // }    
 
     public function userlogout(Request $request)
     {
